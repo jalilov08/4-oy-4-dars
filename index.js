1c3913e0727d9b790201630e81e9d82cc24df9c0
jalilov08 <jalilovnozimjon054@gmail.com>
2023-12-01 22:01:54 +0500

    Homework in algarithm

---
+++ index.js
@@ -0,0 +1,19 @@
+/**  Masala
+      let a = +prompt ("Ozingizda bor pulni qiymatini kiriting.");
+      let d = 11000.34;
+      let y = 12354.03;
+      let plane = d * 500;
+      let hotel = d * 250;
+      let museum = y * 120;
+
+      let dollar = plane + hotel;
+      let yevro = museum;
+      let all = dollar + yevro;
+
+      if (a > all) {
+        console.log("Oq yo'l Alisher");
+      }
+      else {
+        console.log("Alisher ozgina sabr qilish kerak");
+      }
+ */
